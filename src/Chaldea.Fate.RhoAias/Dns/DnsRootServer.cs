3702030cba5f4d172a177eb81148ca6bdc17ab8b
zhaowenkai <799480165@qq.com>
2024-07-30 09:53:13 +0800

    add dns domain

---
+++ src/Chaldea.Fate.RhoAias/Dns/DnsRootServer.cs
@@ -0,0 +1,14 @@
+﻿namespace Chaldea.Fate.RhoAias;
+
+public class DnsRootServer
+{
+    public Guid Id { get; set; }
+
+    public string HostName { get; set; } = default!;
+
+    public string Manager { get; set; } = default!;
+
+    public string IPV4Address { get; set; } = default!;
+
+    public string IPV6Address { get; set; } = default!;
+}
---
+++ src/Chaldea.Fate.RhoAias/Dns/DnsZone.cs
@@ -0,0 +1,42 @@
+﻿namespace Chaldea.Fate.RhoAias;
+
+public class DnsZone
+{
+    public Guid Id { get; set; }
+    public string Domain { get; set; } = default!;
+    public ICollection<DnsRecord>? ResourceRecords { get; set; }
+}
+
+public enum DnsRecordType
+{
+    A = 1,
+    NS = 2,
+    CNAME = 5,
+    SOA = 6,
+    MB = 7,
+    MG = 8,
+    MR = 9,
+    NULL = 10,
+    WKS = 11,
+    PTR = 12,
+    HINFO = 13,
+    MINFO = 14,
+    MX = 15,
+    TXT = 16,
+    AAAA = 28,
+}
+
+public class DnsRecord
+{
+    public Guid Id { get; set; }
+
+    public string SubZoneName { get; set; } = default!;
+
+    public DnsRecordType ResourceType { get; set; }
+
+    public int Ttl { get; set; } = 3600;
+
+    public ushort ResourceClasses { get; set; } = 1;
+
+    public string? Address { get; set; }
+}
